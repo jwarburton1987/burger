8d8759070b3e94c5b005e6928773eda016ef855a
Jwarburton1987 <jamesforrealestate@gmail.com>
2018-07-26 23:44:18 -0700

    sorry just got home

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name) VALUES ('DOUBLE DOUBLE');
+INSERT INTO burgers (burger_name) VALUES ('BIG MAC');
+
+INSERT INTO burgers (burger_name) VALUES ('CHICKEN BACON BURGER');
+
+
+
